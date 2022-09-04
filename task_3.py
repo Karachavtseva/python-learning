0bf9cc5a90115c5f22201f8faaff48e3af402df9
Karachavtseva <112814160+Karachavtseva@users.noreply.github.com>
2022-09-04 16:30:15 +0100

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,35 @@
+#Task 1
+"""
+name = "Yuliia"
+day_of_week = "Sunday"
+name_and_date = f"Good day {name}! {day_of_week} is a perfect day to learn some python."
+print(name_and_date)
+
+other_way="Good day {}! {} is a perfect day to learn some python."
+print(other_way.format(name,day_of_week))
+
+print("Good day %s %s is a perfect day to learn some python." % (name,day_of_week))
+
+"""
+
+#Task 2
+"""
+name = "Yuliia"
+surname = "Karachavtseva"
+print(name + " " + surname )
+"""
+
+#Task 3
+"""
+a = 2
+b = 3
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a**b)
+print(a%b)
+print(a//b)
+
+"""
+
---
+++ task_4.py
@@ -0,0 +1,39 @@
+#Task 1
+"""x = "helloworld"
+
+print(x[0:2],x[8:10])  #не знаю як видалити пробіл
+"""
+
+#Task2
+"""contact_number = "7568240959"
+if len(contact_number) >10:
+  print("The contact number has to be less or equals 10")
+else:
+  print("The contact number is:", contact_number, "The type is:", type(contact_number), "The length of number is:", len(contact_number))
+"""
+#Task3
+"""
+expression = int(input("What is the answer for 3+5?"))
+if expression == 8:
+  print("You are right!")
+else:
+  print("Think twice")
+"""
+
+#Task4
+"""
+name = "yuliia"
+name_from_user = input("Write the name?")
+name_from_user.lower()
+if name == name_from_user.lower():
+  print("good job")
+else:
+  print("Try again")
+
+"""
+
+
+
+
+
+
